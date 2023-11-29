20b95c773aca48241d12f177009781909d7de90d
mllangon <mllangon@myuax.com>
2023-11-29 19:20:49 +0100

    2.3

--- 2_Secuencias.cpp
+++ 2_Secuencias.cpp
@@ -6,13 +6,18 @@ using namespace std;
 int main() {
     list<double> decimallist = {1.1, 2.2, 3.3, 4.4, 5.5};
     stack<double, std::list<double>> decimalStack(decimallist);
-    cout << "Top elemento: " << decimalStack.top() << endl;
+    cout << "Elemento top: " << decimalStack.top() << endl;
 
     decimalStack.pop();
 
     decimalStack.push(6.6);
-    cout << "Top elemento: " << decimalStack.top() << endl;
+    cout << "Nuevo elemento top: " << decimalStack.top() << endl;
 
     cout << "Elementos restantes: " << endl;
 
+    while (!decimalStack.empty()) {
+        std::cout << decimalStack.top() << " ";
+        decimalStack.pop();
+    }
+return 0;
 }
