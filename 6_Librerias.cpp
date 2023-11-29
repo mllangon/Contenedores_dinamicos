510f7cb27b89529595b607e3172feededb36cb53
mllangon <mllangon@myuax.com>
2023-11-29 19:56:10 +0100

    6.3

--- 6_Librerias.cpp
+++ 6_Librerias.cpp
@@ -5,3 +5,13 @@ using namespace std;
 
 int main() {
     string numberStr = "1234";
+
+    try {
+        int number = boost::lexical_cast<int>(numberStr);
+        cout << "Número convertido: " << number << endl;
+    } catch (const boost::bad_lexical_cast& e) {
+        cerr << "Error de conversión: " << e.what() << endl;
+    }
+
+    return 0;
+}
