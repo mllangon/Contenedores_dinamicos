030e469a23a0fe8b89c9c9d10754d571516cf4fd
mllangon <mllangon@myuax.com>
2023-11-29 19:04:58 +0100

    1.2

--- 1_Iteradores.cpp
+++ 1_Iteradores.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
 int main() {
-    std::vector<int> numeros;
+    vector<int> numeros;
     int numero;
 
-    std::cout << "Introduce los números que quieras añadir al vector (0 para terminar):" << std::endl;
+    cout << "Introduce los números que quieras añadir al vector (0 para terminar):" << std::endl;
+
+    while(true){
+    cin >> numero;
+        if(numero == 0){
+            break;
+    }
+        numeros.push_back(numero);
+    }
+
+    cout << ""
+}
