a464267701e011de94353489784cbe0d96c1c96c
mllangon <mllangon@myuax.com>
2023-11-29 19:40:01 +0100

    4.2

--- 4_Calculo.cpp
+++ 4_Calculo.cpp
@@ -13,5 +13,29 @@ int main() {
     valarray<double> division = numeros / 2.0;
     valarray<double> raizCuadrada = sqrt(numeros);
 
+    cout << "Numeros originales: ";
+    for (double num : numeros) cout << num << " ";
+    cout << endl;
 
+    cout << "Suma: ";
+    for (double num : suma) cout << num << " ";
+    cout << endl;
+
+    cout << "Resta: ";
+    for (double num : resta) cout << num << " ";
+    cout << endl;
+
+    cout << "Multiplicacion: ";
+    for (double num : multiplicacion) cout << num << " ";
+    cout << endl;
+
+    cout << "Division: ";
+    for (double num : division) cout << num << " ";
+    cout << endl;
+
+    cout << "Raiz cuadrada: ";
+    for (double num : raizCuadrada) cout << num << " ";
+    cout << endl;
+
+    return 0;
 }
